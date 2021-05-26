41e6707b878dc5c60aa7009276e9a36fee4ac212
jeasonW55 <977659635@qq.com>
2021-05-26 22:05:58 +0800

    删除掉没意义的布局，准备写一个app

--- app/src/main/java/com/jeason/imageload/MainActivity.java
+++ app/src/main/java/com/jeason/imageload/MainActivity.java
@@ -24,95 +24,12 @@ import threadPool.ThreadPool;
 
 import static download.base.RequestMethod.GET;
 
-public class MainActivity extends AppCompatActivity implements ICallback<Bitmap>, UnitUrl, View.OnClickListener {
-
-    private ImageView mLoadImageView;
-
-    private Handler mUIHandler;
-
-    private Runnable mShowImage = new Runnable() {
-        int index = 0;
-        @Override
-        public void run() {
-            request(index++);
-            if(index == imageUrlList.size()){
-                index = 0;
-            }
-            mUIHandler.postDelayed(this, 1000);
-        }
-    };
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mUIHandler = new Handler(getMainLooper());
-        initView();
-        initImageUrl();
-
-        mUIHandler.postDelayed(mShowImage, 0);
-
-        mLoadImageView.setOnClickListener(this);
     }
 
-    private void initView() {
-        mLoadImageView = findViewById(R.id.load_image);
-    }
-
-    private void request(int i) {
-        AbstractHttpRequest fileRequest = new RequestBuilder<FileRequest, Bitmap>(FileRequest.class)
-                .init()
-                .setRequestUrl(imageUrlList.get(i))
-                .setRequestCallback(MainActivity.this)
-                .setRequestMethod(GET)
-                .builder();
-        ThreadPool.getSingleThreadPool(new Runnable() {
-            @Override
-            public void run() {
-                ByteArrayOutputStream byteArrayOutputStream = fileRequest.connect();
-                byte[] bitmapByte = byteArrayOutputStream.toByteArray();
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapByte,0, bitmapByte.length);
-                mUIHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        onSuccess(bitmap);
-                    }
-                });
-            }
-        });
-    }
-
-
-    @Override
-    public void onSuccess(Bitmap bitmap) {
-        mLoadImageView.setImageBitmap(bitmap);
-    }
-
-    @Override
-    public void onFailure(Exception e) {
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mUIHandler.removeCallbacks(mShowImage);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mUIHandler.removeCallbacks(mShowImage);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mUIHandler.removeCallbacks(mShowImage);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this, mLoadImageView.toString(), Toast.LENGTH_LONG).show();
-    }
 }
